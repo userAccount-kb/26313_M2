e62644d8d775230074a03ca889773f9866a264f7
Kashai <sweetpea617@outlook.com>
2021-01-19 11:37:14 -0500

    adjustments made to code to validate input; needed - code refinement and comments

--- M2ToDoApp/src/M2ToDoApp.java
+++ M2ToDoApp/src/M2ToDoApp.java
@@ -2,38 +2,62 @@ import java.util.Scanner;
 
 public class M2ToDoApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to your personal to-do list!");
-        Scanner userInput = new Scanner(System.in);
-        String action = "", item = "";
         ToDoList user1 = new ToDoList();
-        
-        Scanner userResponse = new Scanner (System.in);
-        
-        while (!action.equalsIgnoreCase("q")) {
-            System.out.println("You can \"add\", \"delete\", or \"view all\" items. " 
-            + "What would you like to do? \n(press q to quit)");
-
-            try{
-                action = userInput.nextLine();
-            } catch (Exception e) {
-                System.out.println("Something went terribly wrong. Error: " + e);
-                break;
-            }
-
-            //input validation
-            if (action.equalsIgnoreCase("add")) {
-                System.out.println("What item would you like to add?");
-                item = userResponse.nextLine();
-                user1.addItem(item);
-            } else if (action.equalsIgnoreCase("delete")) {
-                System.out.println("Which item would you like to delete?");
-                item = userResponse.nextLine();
-                user1.deleteItem(item);
-            } else if (action.equalsIgnoreCase("view all")) {
-                user1.viewAll();
-            } else {
-                System.out.println("Invalid entry!\n");
-            }
+        System.out.println("Welcome to your personal to-do list!");
+        boolean quit = false;
+        while (quit == false) {
+            quit = runList(user1);
         }
+        
+    }
+
+    public static boolean runList(ToDoList user1) {
+        String action = "", item = "", answer = "";
+        boolean itemExists = false;
+
+        Scanner userInput = new Scanner(System.in), userResponse = new Scanner (System.in);
+        
+        System.out.println("\nYou can \"add\", \"delete\", or \"view all\" items. " 
+        + "What would you like to do? \n(press q to quit)");
+
+        try{
+            action = userInput.nextLine();
+        } catch (Exception e) {
+            System.out.println("Something went terribly wrong. Error: " + e);
+        }
+
+        //input validation
+        if (action.equalsIgnoreCase("add")) {
+            System.out.println("What item would you like to add?");
+            item = userResponse.nextLine();
+            user1.addItem(item);
+        } else if (action.equalsIgnoreCase("delete")) {
+            System.out.println("Which item would you like to delete?");
+            item = userResponse.nextLine();
+            itemExists = user1.verifyItem(item);
+
+            //verify deletion
+            if (itemExists == false) {
+                System.out.println("That item does not exist.");
+            } else {
+                System.out.println("Are you sure you want to delete \"" + item + "\"?");
+                answer = userResponse.next();
+
+                if (answer.equalsIgnoreCase("yes")) { //delete
+                    user1.deleteItem();
+                } else {
+                    System.out.println("Deletion cancelled!");
+                }
+            }
+        } else if (action.equalsIgnoreCase("view all")) {
+            user1.viewAll();
+        } else {
+            System.out.println("Invalid entry!\n");
+        }
+        
+        if (action.equalsIgnoreCase("q")) {
+            return true;
+        }
+        return false;
     }
 }
--- M2ToDoApp/src/ToDoList.java
+++ M2ToDoApp/src/ToDoList.java
@@ -1,49 +1,37 @@
 import java.util.*;
 
 public class ToDoList {
-    //String item;
+    private int index = 0;
     private LinkedList<String> userList = new LinkedList<>();
 
     public void addItem(String item) {
         userList.add(item);
     }
 
-    public void deleteItem(String selectedItem) {
-        Scanner userResponse = new Scanner(System.in);
-        String answer;
-        int index = 0, count = 0;
-        boolean itemExists = false;
+    public void deleteItem() {
+        userList.remove(index);
 
-        //confirm item exists
+        System.out.println("Deletion successful!\n");
+    }
+
+    public boolean verifyItem(String selectedItem) {
+        boolean itemExists = false;
+        int count = 0;
+
         for (String list : userList) {
             if (selectedItem.equalsIgnoreCase(list)) {
                 index = count;
-                System.out.print("Are you sure you wish to delete \"" + list + "\"?");
+                itemExists = true;
             }
             count++;
         }
-        
-        //verify deletion
-        if (itemExists) {
-            System.out.println("Are you sure you want to delete \"" + selectedItem + "\"?");
-            if (userResponse.next().equals("yes") || userResponse.next().equals("no"))  {
-                answer = userResponse.next();
 
-                if (answer.equalsIgnoreCase("yes")) { //delete
-                    userList.remove(index);
-                }
-            }
-        } else {
-            System.out.println("That item does not exist.");
-        }
-
-        userResponse.close();
+        return itemExists;
     }
-
 
     public void viewAll() {
         for (String list : userList) {
-            System.out.print(list + "\n");
+            System.out.print("\n" + list);
         }
     }
 }
